88c7800ea0ca4680330930d285c5aaaad5401d37
ghost general <awotundun@gmail.com>
2019-01-24 11:18:06 +0100

    video mining Application

---
+++ app.js
@@ -0,0 +1,27 @@
+
+var express = require('express');
+var formidable = require('formidable');
+
+var app = express();
+
+app.get('/', function (req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function (req, res){
+    var form = new formidable.IncomingForm();
+
+    form.parse(req);
+
+    form.on('fileBegin', function (name, file){
+        file.path = __dirname + '/uploads/' + file.name;
+    });
+
+    form.on('file', function (name, file){
+        console.log('Uploaded ' + file.name);
+    });
+
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3000);
